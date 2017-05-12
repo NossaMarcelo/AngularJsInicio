aed8b2d3e1504ac8ab6f544ef6ee9d75c55b847b
Marcelinho Programador <NossaMarcelo@gmail.com>
2017-05-12 13:59:20 -0300

    #changeLogo Todo teste

--- src/app/br-examples-todo/todo/todo.component.ts
+++ src/app/br-examples-todo/todo/todo.component.ts
@@ -10,13 +10,36 @@ export class TodoComponent implements OnInit {
   newTodo: string;
   todos: any;
   todoObj: any;
-
+//dsadas
 
   constructor() {
     this.newTodo='';
     this.todos = [];
     
    }
+
+   addTodo(event) {
+     this.todoObj= {
+       newTodo: this.newTodo,
+       completed: false
+     }
+   
+   this.todos.push(this.todoObj);
+   this.newTodo='';
+   event.preventDefault();
+  }
+  deleteTodo(index){
+    this.todos.splice(index, 1);
+  }
+
+  deleteSelectedTodos(){
+    for(var i=(this.todos.lenght -1);i>-1;i--){
+      if(this.todos[i].completed){
+        this.todos.splice(i,1);
+      }
+    }
+  }
+
 
   ngOnInit() {
   }
